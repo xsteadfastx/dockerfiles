e9f32d541e8dfd43a18e2f0322816b57776d9ece
Marvin Steadfast <marvin@xsteadfastx.org>
2016-11-10 12:39:31 +0100

    mopidy: icecast image cleaning

--- mopidy/icecast/root/bin/entrypoint.sh
+++ mopidy/icecast/root/bin/entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$1" = "icecast" ];then
   mkfifo -m 600 /tmp/logpipe
   cat <> /tmp/logpipe 1>&2 &
   chown icecast /tmp/logpipe
-  exec su-exec icecast icecast -c /etc/icecast2/icecast.xml
+  exec su-exec icecast icecast -c /etc/icecast.xml
 fi
 
 exec "$@"
