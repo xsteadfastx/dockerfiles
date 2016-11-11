4c8fec70f751639b9c09ec0dd242da43c6b1d718
Marvin Steadfast <marvin@xsteadfastx.org>
2016-11-11 14:52:31 +0100

    mopidy: still not happy

--- mopidy/mopidy/root/bin/entrypoint.sh
+++ mopidy/mopidy/root/bin/entrypoint.sh
@@ -2,7 +2,14 @@
 
 if [ "$1" = "mopidy" ];then
   chown mopidy:audio /var/lib/mopidy
+
+  # print full config
+  gosu mopidy mopidy config
+
+  # scan for local music
   gosu mopidy mopidy local scan
+
+  # run mopidy
   exec gosu mopidy mopidy
 fi
 
