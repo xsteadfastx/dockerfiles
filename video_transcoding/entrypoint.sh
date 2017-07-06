8b19ecbe8a8a6d9ca663795b48088069e96d9e7c
Marvin Steadfast <marvin@xsteadfastx.org>
2017-07-06 12:56:06 +0200

    video_transcoding: moved to ubuntu

--- video_transcoding/entrypoint.sh
+++ video_transcoding/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 if [ "$1" = "video_transcoding" ];then
         groupadd -g $GROUPID trans
-        useradd -g $GROUPID -u $USERID -s /bin/zsh -m trans
+        useradd -g $GROUPID -u $USERID -s /bin/zsh -m -G cdrom trans
         curl https://raw.githubusercontent.com/xsteadfastx/batcave/master/roles/zsh/files/zshrc -o /home/trans/.zshrc
-        git clone https://github.com/zsh-users/antigen.git /home/trans/.antigen
+        git clone --depth 1 https://github.com/zsh-users/antigen.git /home/trans/.antigen
         chown -R trans:trans /home/trans
         gosu trans /bin/zsh -c "source /home/trans/.zshrc && antigen update"
         exec gosu trans /bin/zsh
