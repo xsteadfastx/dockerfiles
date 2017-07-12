6fa7b0f0d56493cc7d4fabef7b0340e5dd40c1aa
Marvin Steadfast <marvin@xsteadfastx.org>
2017-07-12 09:39:39 +0200

    further

--- video_transcoding/rip_show/rip_show.py
+++ video_transcoding/rip_show/rip_show.py
@@ -13,13 +13,25 @@ from typing import List, Tuple, Union
 import click
 
 
+LANGUAGES = {
+    'german': {
+        '1': 'de',
+        '2': 'deu'
+    },
+    'english': {
+        '1': 'en',
+        '2': 'eng'
+    }
+}
+
+
 def trackstr_to_list_of_int(trackstr: str) -> List[int]:
     """Converts a str with tracknumbers to a list of tracknumbers."""
-
     return [int(i) for i in trackstr.split(' ')]
 
 
 def get_subtitle_id(language: str, track_info: str) -> Union[str, None]:
+    """Searches for subtitle id."""
     search_sid = re.search(
         'subtitle\s\(\ssid\s\):\s(\d)\slanguage:\s{}'.format(language),
         track_info
@@ -36,8 +48,8 @@ def get_subtitle_id(language: str, track_info: str) -> Union[str, None]:
     '--language', '-l',
     help='Languages to use.',
     multiple=True,
-    type=click.Choice(['de', 'en']),
-    default=('de', 'en')
+    type=click.Choice(list(LANGUAGES.keys())),
+    default=tuple(LANGUAGES.keys())
 )
 @click.option(
     '--keep', '-k',
@@ -55,10 +67,8 @@ def main(language: Tuple[str], keep: bool, temp: str):
     else:
         tmpdir = temp
 
-    dvd_image = path.join(tmpdir, 'DVD')
-
     # copy dvd to harddrive
-    if not path.exists(dvd_image):
+    if not path.exists(path.join(tmpdir, 'DVD')):
 
         click.secho('---> ripping dvd', fg='black', bg='white')
         subprocess.run(
@@ -78,8 +88,9 @@ def main(language: Tuple[str], keep: bool, temp: str):
         [
             'transcode-video',
             '--scan',
-            dvd_image
+            'DVD'
         ],
+        cwd=tmpdir,
         check=True
     )
 
@@ -93,25 +104,32 @@ def main(language: Tuple[str], keep: bool, temp: str):
         track_info_raw = subprocess.run(
             [
                 'mplayer',
-                '-dvd-device', dvd_image,
+                '-dvd-device', 'DVD',
                 'dvd://{}'.format(track),
                 '-vo', 'null',
                 '-ao', 'null',
                 '-frames', '0',
                 '-identify'
             ],
+            cwd=tmpdir,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             check=True
         )
 
-        track_info = track_info_raw.stdout.decode('utf-8')
+        if isinstance(track_info_raw.stdout, bytes):
+            track_info = track_info_raw.stdout.decode('utf-8')
+        else:
+            track_info = track_info_raw.stdout
 
         for lang in language:
 
-            sid = get_subtitle_id(lang, track_info)
+            sid = get_subtitle_id(LANGUAGES[lang]['1'], track_info)
 
             if sid:
+
+                file_base = '{}-{}'.format(track, lang)
+
                 click.secho(
                     '---> ripping subtitle {}'.format(lang),
                     fg='black', bg='white'
@@ -119,17 +137,18 @@ def main(language: Tuple[str], keep: bool, temp: str):
                 subprocess.run(
                     [
                         'mencoder',
-                        '-dvd-device', dvd_image,
+                        '-dvd-device', 'DVD',
                         'dvd://{}'.format(track),
                         '-nosound',
                         '-ovc', 'frameno',
                         '-o', '/dev/null',
-                        '-slang', lang,
+                        '-slang', LANGUAGES[lang]['1'],
                         '-sid', sid,
                         '-vobsuboutindex', '0',
-                        '-vobsuboutid', lang,
-                        '-vobsubout', lang
+                        '-vobsuboutid', LANGUAGES[lang]['1'],
+                        '-vobsubout', file_base
                     ],
+                    cwd=tmpdir,
                     check=True
                 )
 
@@ -140,8 +159,9 @@ def main(language: Tuple[str], keep: bool, temp: str):
                 subprocess.run(
                     [
                         'vobsub2srt',
-                        lang
+                        file_base
                     ],
+                    cwd=tmpdir,
                     check=True
                 )
 
@@ -150,6 +170,30 @@ def main(language: Tuple[str], keep: bool, temp: str):
                     'could not find subtitle language',
                     fg='red', bg='white'
                 )
+
+        click.secho(
+            '---> ripping video',
+            fg='black', bg='white'
+        )
+
+        video_rip_cmd = [
+            'transcode-video',
+            '--title', str(track),
+            '--main-audio={}'.format(LANGUAGES[language[0]]['2'])
+        ]
+
+        for lang in language[1:]:
+            video_rip_cmd.append(
+                '--add-audio={}'.format(LANGUAGES[lang]['2'])
+            )
+
+        video_rip_cmd.append('DVD')
+
+        subprocess.run(
+            video_rip_cmd,
+            cwd=tmpdir,
+            check=True
+        )
 
     # clean it up
     if not keep:
