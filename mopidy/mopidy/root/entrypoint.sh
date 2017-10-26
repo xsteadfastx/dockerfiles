b03b17db0ca2d8144949a4e6af80520e49aa28b7
Marvin Steadfast <marvin@xsteadfastx.org>
2017-10-26 08:07:12 +0200

    mopidy: added local sqlite and entrypoint with scan command

--- mopidy/mopidy/root/entrypoint.sh
+++ mopidy/mopidy/root/entrypoint.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-if [ "$1" = "mopidy" ];then
+if [ "$1" = "start" ];then
         exec su-exec mopidy mopidy
+fi
+
+if [ "$1" = "scan" ];then
+        exec su-exec mopidy mopidy local scan
+fi
+
+if [ "$1" = "clear" ];then
+        exec su-exec mopidy mopidy local clear
 fi
 
 exec "$@"
