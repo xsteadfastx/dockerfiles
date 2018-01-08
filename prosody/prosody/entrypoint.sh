72b0cac139d2ce4a2c3e9433fb40d72e03dafc21
Marvin Steadfast <marvin@xsteadfastx.org>
2018-01-08 11:45:48 +0100

    prosody: letsencrypt and chown

--- prosody/prosody/entrypoint.sh
+++ prosody/prosody/entrypoint.sh
@@ -3,6 +3,10 @@
 set -e
 
 if [ "$1" = "start" ];then
+        chown -R prosody:prosody /etc/prosody
+        chown -R prosody:prosody /var/lib/prosody
+        chown -R prosody:prosody /var/log/prosody
+        chown -R prosody:prosody /usr/lib/prosody-modules
         exec su-exec prosody prosody
 fi
 
