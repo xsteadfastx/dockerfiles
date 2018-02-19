07e9551d89f1dfbbb23303faaf465ad37b2e980a
Marvin Steadfast <marvin@xsteadfastx.org>
2018-02-19 14:56:09 +0100

    nextcloud: entrypoint deletes user and group if exists

--- nextcloud/nginx/entrypoint.sh
+++ nextcloud/nginx/entrypoint.sh
@@ -4,7 +4,13 @@ set -e
 
 if [ "$1" = "nginx" ];then
         echo "---> create user"
-        deluser xfs
+        if grep -q :33: /etc/passwd > /dev/null 2>&1; then
+                sed -i "/:33:/d" /etc/passwd
+                sed -i "/:33:/d" /etc/shadow
+        fi
+        if grep -q :33: /etc/group > /dev/null 2>&1; then
+                sed -i "/:33:/d" /etc/group
+        fi
         addgroup -S -g 33 www-data
         adduser -S -h /var/www/html -H -s /sbin/nologin -u 33 -G www-data www-data
 
