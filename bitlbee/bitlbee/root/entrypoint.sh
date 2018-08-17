0d25695fda6878ae5a12001a9ffb2126e0b13c3e
Marvin Steadfast <marvin@xsteadfastx.org>
2018-08-17 08:42:59 +0200

    bitlbee: based on alpine again

--- bitlbee/bitlbee/root/entrypoint.sh
+++ bitlbee/bitlbee/root/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$1" = "bitlbee" ];then
     chown -R bitlbee:bitlbee /var/lib/bitlbee
     chown -R bitlbee:bitlbee /var/run/bitlbee
-    exec gosu bitlbee bitlbee -F -n -v -P /var/run/bitlbee/bitlbee.pid
+    exec su-exec bitlbee bitlbee -F -n -v -P /var/run/bitlbee/bitlbee.pid
 fi
 
 exec "$@"
