39bde3445cfb980a10dc557ed85d653718c3a30b
Marvin Steadfast <marvin@xsteadfastx.org>
2018-08-22 08:17:14 +0200

    bitlbee: chown on bitlbee conf

--- bitlbee/bitlbee/root/entrypoint.sh
+++ bitlbee/bitlbee/root/entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 if [ "$1" = "bitlbee" ];then
+    chown -R bitlbee:bitlbee /etc/bitlbee/bitlbee.conf
     chown -R bitlbee:bitlbee /var/lib/bitlbee
     chown -R bitlbee:bitlbee /var/run/bitlbee
     exec su-exec bitlbee bitlbee -F -n -v -P /var/run/bitlbee/bitlbee.pid
